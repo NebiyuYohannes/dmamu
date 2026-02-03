403b9cf404caa736031ad318bdb1a5a2c55c9f66
Eyob Mulugeta <eyobbmulugeta@gmail.com>
2026-02-03 03:46:05 +0300

    REFACTOR: Forget password, Resend activation code and verify otp

--- backend/habsify/urls.py
+++ backend/habsify/urls.py
@@ -17,6 +17,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+
+admin.site.site_header = "Habsify Admin"
+admin.site.site_title = "Habsify Admin Portal"
+admin.site.index_title = "Welcome to Habsify Administration"
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/', include('djoser.urls')),
