cae7133e8e55c75430a5ff612935d00cc06e639c
Eyob Mulugeta <eyobbmulugeta@gmail.com>
2026-02-05 22:35:42 +0300

    Refactor: Delete Profile Model For Clear

--- backend/habsify/urls.py
+++ backend/habsify/urls.py
@@ -17,15 +17,14 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-
 admin.site.site_header = "Habsify Admin"
 admin.site.site_title = "Habsify Admin Portal"
 admin.site.index_title = "Welcome to Habsify Administration"
-
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.urls.jwt')),
-    path('auth/', include('accounts.urls')),
+    path('accounts/', include('accounts.urls')),
 ]
+
