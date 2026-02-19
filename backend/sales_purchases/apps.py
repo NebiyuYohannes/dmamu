d4267ba23e9e725f85ecf0e52dca8cb5ebf10c84
Eyob Mulugeta <eyobbmulugeta@gmail.com>
2026-02-19 07:21:12 +0300

    Refactor: sales/purchase

--- backend/sales_purchases/apps.py
+++ backend/sales_purchases/apps.py
@@ -3,3 +3,7 @@ from django.apps import AppConfig
 
 class SalesPurchasesConfig(AppConfig):
     name = 'sales_purchases'
+    verbose_name = "Sales & Purchases"
+
+    def ready(self):
+        import sales_purchases.signals  
