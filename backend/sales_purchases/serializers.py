0af915244dce986ad7bf1521ada7c70815b61f03
Eyob Mulugeta <eyobbmulugeta@gmail.com>
2026-02-20 04:08:37 +0300

    fix(sales_purchase): resolve reference field error

--- backend/sales_purchases/serializers.py
+++ backend/sales_purchases/serializers.py
@@ -1,86 +1,107 @@
 from rest_framework import serializers
-from .models import Sale, Purchase,Customer
-from inventory.models import Item
+from .models import Sale, Purchase,PaymentStatus
+from finance.models import Account, Transaction
+from django.db.models import Sum
 
 class SaleSerializer(serializers.ModelSerializer):
-    customer = serializers.PrimaryKeyRelatedField(queryset=Customer.objects.all())
-    customer_name = serializers.CharField(source='customer.name', read_only=True)
+    # customer = serializers.CharField(source='customer.name')
+    balance = serializers.SerializerMethodField() 
 
     class Meta:
         model = Sale
-        fields = ['id', 'customer', 'customer_name', 'item', 'quantity', 'unit_price',
-                  'total', 'date', 'notes', 'status', 'payment_method', 'account']
-        read_only_fields = ['total', 'date']
+        fields = ['id', 'customer', 'item', 'quantity', 'unit_price', 'total', 'date', 'notes', 'status', 'payment_method', 'account', 'balance']
+        read_only_fields = ['total', 'date', 'balance']
 
     def validate(self, data):
         data['total'] = data['quantity'] * data['unit_price']
 
+        status = data.get('status')
         payment_method = data.get('payment_method')
         account = data.get('account')
 
-        if not account:
-            raise serializers.ValidationError("Account ID is required for both cash and bank_transfer.")
-
-        if payment_method == 'bank_transfer':
-            if account.account_type != 'bank':
-                raise serializers.ValidationError("Bank Transfer requires a Bank account.")
-
-        elif payment_method == 'cash':
-            if account.account_type != 'cash':
-                raise serializers.ValidationError("Cash payment requires a Cash account.")
-
-        else:
-            raise serializers.ValidationError("Payment method must be 'cash' or 'bank_transfer'.")
-
-        item = data.get('item')
-        requested_qty = data.get('quantity')
-
-        if item and requested_qty is not None:
-            if item.current_stock < requested_qty:
+        if status == PaymentStatus.UNPAID:
+            if account or payment_method:
                 raise serializers.ValidationError(
-                    f"You don't have enough {item.name} items. "
-                    f"Available: {item.current_stock}, Requested: {requested_qty}"
+                    "For 'UNPAID' status, account and payment_method must be empty."
                 )
 
+
+        if status == PaymentStatus.PAID:
+            if not account or not payment_method:
+                raise serializers.ValidationError("For 'paid' status, account and payment_method are required.")
+            if payment_method == 'bank_transfer':
+                if account.account_type != 'bank':
+                    raise serializers.ValidationError("Bank Transfer requires a Bank account.")
+                if account.account_number in ['', None]:
+                    raise serializers.ValidationError("Choose proper account with number.")
+                if account.balance < data['total']:
+                    raise serializers.ValidationError("Insufficient balance in selected account.")
+            elif payment_method == 'cash':
+                if account.account_type != 'cash':
+                    raise serializers.ValidationError("Cash payment requires a Cash account.")
+                if account.balance < data['total']:
+                    raise serializers.ValidationError("Insufficient balance in selected account.")
+            else:
+                raise serializers.ValidationError("Payment method must be 'cash' or 'bank_transfer'.")
+
+        if status == PaymentStatus.PARTIAL:
+            if not account or not payment_method:
+                raise serializers.ValidationError("For 'partial' status, account and payment_method are required, and you must add transactions separately.")
+
         return data
+
+    def get_balance(self, obj):
+        paid = obj.transactions.aggregate(Sum('amount'))['amount__sum'] or 0
+        return obj.total - paid
 
 class PurchaseSerializer(serializers.ModelSerializer):
     supplier_name = serializers.CharField(source='supplier.name', read_only=True)
     item_name = serializers.CharField(source='item.name', read_only=True)
+    balance = serializers.SerializerMethodField()
 
     class Meta:
         model = Purchase
         fields = ['id', 'supplier', 'supplier_name', 'item', 'item_name', 'quantity',
-                  'unit_price', 'total', 'date', 'notes', 'status', 'payment_method', 'account']
-        read_only_fields = ['total', 'date']
+                  'unit_price', 'total', 'date', 'notes', 'status', 'payment_method', 'account', 'balance']
+        read_only_fields = ['total', 'date', 'balance'] 
 
     def validate(self, data):
         data['total'] = data['quantity'] * data['unit_price']
 
+        status = data.get('status')
         payment_method = data.get('payment_method')
         account = data.get('account')
 
-        if not account:
-            raise serializers.ValidationError("Account ID is required for both cash and bank_transfer.")
+        if status == PaymentStatus.UNPAID:
+            if account or payment_method:
+                raise serializers.ValidationError(
+                    "For 'UNPAID' status, account and payment_method must be empty."
+                )
 
-        if payment_method == 'bank_transfer':
-            if account.account_type != 'bank':
-                raise serializers.ValidationError("Bank Transfer requires a Bank account.")
-            if account.account_number in ['', None]:
-                raise serializers.ValidationError("Choose proper .")
-            if account.balance < data['total']:
-                raise serializers.ValidationError("Insufficient balance in selected account.")
+        if status == PaymentStatus.PAID:
+            if not account or not payment_method:
+                raise serializers.ValidationError("For 'paid' status, account and payment_method are required.")
+            if payment_method == 'bank_transfer':
+                if account.account_type != 'bank':
+                    raise serializers.ValidationError("Bank Transfer requires a Bank account.")
+                if account.account_number in ['', None]:
+                    raise serializers.ValidationError("Choose proper account with number.")
+                if account.balance < data['total']:
+                    raise serializers.ValidationError("Insufficient balance in selected account.")
+            elif payment_method == 'cash':
+                if account.account_type != 'cash':
+                    raise serializers.ValidationError("Cash payment requires a Cash account.")
+                if account.balance < data['total']:
+                    raise serializers.ValidationError("Insufficient balance in selected account.")
+            else:
+                raise serializers.ValidationError("Payment method must be 'cash' or 'bank_transfer'.")
 
-        elif payment_method == 'cash':
-            if account.account_type != 'cash':
-                raise serializers.ValidationError("Cash payment requires a Cash account.")
-            if account.balance < data['total']:
-                raise serializers.ValidationError("Insufficient balance in selected account.")
-
-        else:
-            raise serializers.ValidationError("Payment method must be 'cash' or 'bank_transfer'.")
+        if status == PaymentStatus.PARTIAL:
+            if not account or not payment_method:
+                raise serializers.ValidationError("For 'partial' status, account and payment_method are required, and you must add transactions separately.")
 
         return data
 
-    def create(self, validated_data):
-            return super().create(validated_data)
+    def get_balance(self, obj):
+        paid = obj.transactions.aggregate(Sum('amount'))['amount__sum'] or 0
+        return obj.total - paid
