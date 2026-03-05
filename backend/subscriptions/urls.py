52a1d257ad57f04e47a6cac6d339f9ca789c74d2
Eyob Mulugeta <eyobbmulugeta@gmail.com>
2026-03-06 02:17:02 +0300

    SubscriptionViewSet fix(permissions): update BankAccountViewSet to remove HasActiveSubscription permission

--- backend/subscriptions/urls.py
+++ backend/subscriptions/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import (
-    SubscriptionPlanViewSet, SubscriptionViewSet)
+    SubscriptionPlanViewSet, SubscriptionViewSet,
+    PaymentMethodViewSet, BankAccountViewSet)
 
 router = DefaultRouter()
 router.register('plans', SubscriptionPlanViewSet, basename='plan')
 router.register('subscriptions', SubscriptionViewSet, basename='subscription')
-
+router.register('paymentmethod-dropdown', PaymentMethodViewSet, basename='paymentmethod')
+router.register('bankaccount-dropdown', BankAccountViewSet, basename='bankaccount')
 
 
 urlpatterns = [
