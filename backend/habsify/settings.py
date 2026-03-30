1dbcc6dd196757217f3770540155203242d24fc5
Eyob Mulugeta <eyobbmulugeta@gmail.com>
2026-03-30 17:09:55 +0300

    feat(backend): add WhiteNoise middleware for static file handling

--- backend/habsify/settings.py
+++ backend/habsify/settings.py
@@ -64,6 +64,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -110,7 +111,7 @@ DATABASES = {
 if not DEBUG:
     DATABASES["default"].setdefault("OPTIONS", {})["sslmode"] = "require"
 
-
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
 
