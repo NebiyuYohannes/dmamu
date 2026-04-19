1473454b8e3e025e12a6930d3457854bd4114e05
Eyob Mulugeta <eyobbmulugeta@gmail.com>
2026-04-19 23:37:56 +0300

    fix: conditional dynamic import of react-query-devtools

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,21 +2,29 @@ import React from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { Toaster } from 'sonner'
 import App from './App'
 import { AuthProvider } from './context/AuthContext'
 import './index.css'
 
+// React Query client
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
       refetchOnWindowFocus: false,
       retry: 1,
-      staleTime: 5 * 60 * 1000
-    }
-  }
+      staleTime: 5 * 60 * 1000,
+    },
+  },
 })
+
+const ReactQueryDevtools = import.meta.env.DEV
+  ? React.lazy(() =>
+      import('@tanstack/react-query-devtools').then((mod) => ({
+        default: mod.ReactQueryDevtools,
+      }))
+    )
+  : () => null
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
@@ -29,9 +37,7 @@ createRoot(document.getElementById('root')).render(
 
       <Toaster position="top-right" richColors closeButton theme="light" />
 
-      {import.meta.env.DEV && (
-        <ReactQueryDevtools initialIsOpen={false} position="bottom-right" />
-      )}
+      <ReactQueryDevtools initialIsOpen={false} position="bottom-right" />
     </QueryClientProvider>
   </React.StrictMode>
 )
