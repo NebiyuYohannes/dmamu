f2513b9358c8e46d3ca31d6f401de2f991b12a9f
Eyob Mulugeta <eyobbmulugeta@gmail.com>
2026-05-12 13:31:38 +0300

    feat: split settings into base/dev/prod

--- backend/habsify/celery.py
+++ backend/habsify/celery.py
@@ -1,10 +1,8 @@
 import os
 from celery import Celery
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'habsify.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "habsify.settings.dev")
 
-app = Celery('habsify')
-
-app.config_from_object('django.conf:settings', namespace='CELERY')
-
-app.autodiscover_tasks()  
+app = Celery("habsify")
+app.config_from_object("django.conf:settings", namespace="CELERY")
+app.autodiscover_tasks()
