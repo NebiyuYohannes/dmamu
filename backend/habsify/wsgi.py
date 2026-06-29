bcebb922ee0c74afe1642714bd0ee09c43920e08
eyob-sudo <eyob.english@gmail.com>
2026-06-29 18:24:15 +0300

    Update wsgi.py

--- backend/habsify/wsgi.py
+++ backend/habsify/wsgi.py
@@ -12,5 +12,5 @@ WSGI config for habsify project.
 import os
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "habsify.settings.dev")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "habsify.settings")
 application = get_wsgi_application()
