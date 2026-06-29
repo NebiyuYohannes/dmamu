bc4e5bee1a17c7f1fb784622a943d488de0fb7f8
eyob-sudo <eyob.english@gmail.com>
2026-06-29 18:26:14 +0300

    Update __init__.py

--- backend/habsify/__init__.py
+++ backend/habsify/__init__.py
@@ -2,11 +2,3 @@ from .celery import app as celery_app
 from decouple import config
 
 __all__ = ('celery_app',)
-
-
-ENV = config("ENV", default="dev")
-
-if ENV == "prod":
-    from .prod import *
-else:
-    from .dev import *
